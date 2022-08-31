ad2f8a11d1efd6c545b67188b6bb42c12e065c08
rp <rp@example.com>
2022-08-31 13:23:13 +0000

    fix_entropy_no_increase_by_add_epoch

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #Baseline
 #python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-start_from_scratch --batch-size 512 --epochs 200
 
-#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device cpu --experiment resnet-50-test --batch-size 512 --epochs 200 --pretrained
+#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-test --batch-size 512 --epochs 200 --start-epoch 170 --pretrained
 
-#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-start_from_pretrained_50 --batch-size 512 --pretrained --start-epoch 150 --epochs 200
+#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-test --batch-size 512 --epochs 1 -eval_only  --pretrained
 
 #Li
 #python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-from_scratch --batch-size 512  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0
 
-python3 train.py /mnt/ssd/imagenet/ --device cpu --experiment resnet-50-test --batch-size 512  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.1
+python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-test --batch-size 512  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.3  --pretrained --epochs 200 --start-epoch 170
+
+#python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-30-0.3-4.0 --batch-size 512  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 4.0 -entropy_parameter 0.3  --pretrained --epochs 200 --start-epoch 170
+
+#Apply Li
+#python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-test --batch-size 64  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -eval_only --resume output/train/resnet-50-Li-30-new/model182.ckpt -resume_Li output/train/resnet-50-Li-30-new/Li182.ckpt --epochs 1 --cooldown-epochs 0 --start-epoch 0
