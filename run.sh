278a9f170f7041687b5a7680e3234d857a3c6484
rp <rp@example.com>
2022-09-11 12:15:57 +0000

    add_Li_only

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@
 
 
 #Apply Li
-#python3 train.py /mnt/ssd/imagenet/ --device cpu --experiment resnet-test --batch-size 64  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -eval_only --resume output/train/resnet-50-Li-30-ep0.1_lr0.0001-te3.0-fix_sample/model203.ckpt -resume_Li output/train/resnet-50-Li-30-ep0.1_lr0.0001-te3.0-fix_sample/Li203.ckpt --epochs 1 --cooldown-epochs 0 --start-epoch 0
+python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-test --batch-size 256  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -eval_only --pretrained -resume_Li output/train/resnet-50-Li-only_Li_te_3.0_new/Li209.ckpt --epochs 1 --cooldown-epochs 0 --start-epoch 0 --no-aug
 
 #Only train Li
-python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-only_Li_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --pretrained --no-aug --warmup-epochs 0 --lr 0.0 --lr-noise-pct 0.0 --lr-noise-std 0.0  -save_every 3 --start-epoch 170
+#python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-only_Li_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --pretrained --no-aug --warmup-epochs 0 --lr 0.0 --lr-noise-pct 0.0 --lr-noise-std 0.0  -save_every 3 --start-epoch 170 -train_Li_only
 
 #Train model and Li
 #python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --pretrained --no-aug --warmup-epochs 0 -save_every 3 --start-epoch 170
