ade753cea671dc2bb0cf70272c562f042453dce9
rp <rp@example.com>
2022-09-16 21:22:29 +0000

    edit_run.sh_for_baseline

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #Baseline
-#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-start_from_scratch --batch-size 512 --epochs 200
+#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-no_aug_from_scratch --batch-size 512 --epochs 90 --lr 0.2 --warmup-epoch 0 --cooldown-epochs 0 --sched step --no-aug
 
-#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-test --batch-size 512 --epochs 200 --start-epoch 170 --pretrained
+#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-random_crop_from_scratch --batch-size 512 --epochs 90 --lr 0.2 --warmup-epoch 0 --cooldown-epochs 0 --sched step --color-jitter 0.0
 
-#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-test --batch-size 512 --epochs 1 -eval_only  --pretrained
+#python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-aa_from_scratch --batch-size 512 --epochs 90 --lr 0.2 --warmup-epoch 0 --cooldown-epochs 0 --sched step --aa original
+
+python3 train.py /mnt/ssd/imagenet/ --model resnet50 --device tpu-8 --experiment resnet-50-ra_aug_from_scratch --batch-size 512 --epochs 90 --lr 0.2 --warmup-epoch 0 --cooldown-epochs 0 --sched step --aa rand-m9-mstd0.5
+
 
 
 #Apply Li
-python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-test --batch-size 256  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -eval_only --pretrained -resume_Li output/train/resnet-50-Li-only_Li_te_3.0_new/Li209.ckpt --epochs 1 --cooldown-epochs 0 --start-epoch 0 --no-aug
+#python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-test --batch-size 256  --scale 0.8 1.0 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -eval_only -resume_Li output/train/resnet-50-Li-30-ep0.1_lr0.001-te3.0-fix_sample/Li203.ckpt --epochs 1 --cooldown-epochs 0 --start-epoch 0 --no-aug --resume output/train/resnet-50-Li-30-ep0.1_lr0.001-te3.0-fix_sample/model203.ckpt
 
 #Only train Li
 #python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-only_Li_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --pretrained --no-aug --warmup-epochs 0 --lr 0.0 --lr-noise-pct 0.0 --lr-noise-std 0.0  -save_every 3 --start-epoch 170 -train_Li_only
 
 #Train model and Li
-python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --scale 0.8 1.0 --warmup-epochs 0 -save_every 3 --start-epoch 170
+#python3 train.py /mnt/ssd/imagenet/ --device tpu-8 --experiment resnet-50-Li-_te_3.0_new --batch-size 256 -Li_config_path ../InstaAug/InstaAug_module/configs/config_crop_supervised_imagenet.yaml -target_entropy 3.0 -entropy_parameter 0.03  --pretrained --epochs 200 -Li_lr 0.1 -Li_loss crossentropy -batch_amount 0 -train_only --scale 0.8 1.0 --warmup-epochs 0 -save_every 3 --start-epoch 170
